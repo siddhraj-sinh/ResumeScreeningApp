debdeaae61a88f402c51b3ad5a71b6e0b7c833ad
siddhraj rathod <69664213+siddhraj-sinh@users.noreply.github.com>
2022-09-03 20:59:30 +0530

    Update webapp.py

--- webapp.py
+++ webapp.py
@@ -6,9 +6,9 @@ from sklearn.metrics.pairwise import cosine_similarity
 
 st.title("Candidate selection tool")
 
-st.subheader("NLP based resume screening tool")
+st.subheader("NLP based resume screening")
 
-st.caption("Aim of this project is to whether a candidate is qualified for a role based his or her education, experience, and other information captured on their resume. In a nutshell, it's a form of pattern matching between a job's requirements and the qualifications of a candidate based on their resume.")
+st.caption("Aim of this project is to check whether a candidate is qualified for a role based his or her education, experience, and other information captured on their resume. In a nutshell, it's a form of pattern matching between a job's requirements and the qualifications of a candidate based on their resume.")
 
 uploadedJD = st.file_uploader("Upload Job Description", type="pdf")
 
